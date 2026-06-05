86e8f2b1e0306ac2db4cec3bf2d773a42c963f25
Trương Khang <progichi@gmail.com>
2026-06-06 02:05:29 +0700

    Update login and logout feature

--- app/src/main/java/vn/edu/tdc/apptimvieclam/activities/SettingActivity.kt
+++ app/src/main/java/vn/edu/tdc/apptimvieclam/activities/SettingActivity.kt
@@ -3,15 +3,17 @@ package vn.edu.tdc.apptimvieclam.activities
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 import vn.edu.tdc.apptimvieclam.R
 import vn.edu.tdc.apptimvieclam.databinding.SettingLayoutBinding
 
 class SettingActivity : AppCompatActivity() {
-    private lateinit var binding: SettingLayoutBinding
 
+    private lateinit var binding: SettingLayoutBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = SettingLayoutBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -20,25 +22,30 @@ class SettingActivity : AppCompatActivity() {
         )
 
         binding.bottomMenu.menuSearch.setOnClickListener {
-            val intent = Intent(this, SearchActivity::class.java)
-            startActivity(intent)
-
+            startActivity(Intent(this, SearchActivity::class.java))
             finish()
         }
 
         binding.bottomMenu.menuSaved.setOnClickListener {
-            val intent = Intent(this, SavedActivity::class.java)
-            startActivity(intent)
-
+            startActivity(Intent(this, SavedActivity::class.java))
             finish()
         }
 
         binding.bottomMenu.menuHome.setOnClickListener {
-            val intent = Intent(this, HomeActivity::class.java)
-            startActivity(intent)
-
+            startActivity(Intent(this, HomeActivity::class.java))
             finish()
         }
 
+        // Đăng xuất
+        binding.btnLogout.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+
+            val intent = Intent(this, LoginActivity::class.java)
+            intent.flags =
+                Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+
+            startActivity(intent)
+            finish()
+        }
     }
 }
