6ea85658ba6ca7dc12d24789e4781175cf7876eb
Trương Khang <progichi@gmail.com>
2026-06-06 19:52:04 +0700

    Hoàn thành task Code Login, Register và cấu hình Firebase Auth

--- app/src/main/java/vn/edu/tdc/apptimvieclam/activities/SettingActivity.kt
+++ app/src/main/java/vn/edu/tdc/apptimvieclam/activities/SettingActivity.kt
@@ -2,49 +2,46 @@ package vn.edu.tdc.apptimvieclam.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import vn.edu.tdc.apptimvieclam.R
-import vn.edu.tdc.apptimvieclam.databinding.SettingLayoutBinding
 
 class SettingActivity : AppCompatActivity() {
 
-    private lateinit var binding: SettingLayoutBinding
+    private lateinit var btnUpdatePassword: Button
+    private lateinit var btnLogout: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.setting_layout)
 
-        binding = SettingLayoutBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        btnUpdatePassword = findViewById(R.id.btnUpdatePassword)
+        btnLogout = findViewById(R.id.btnLogout)
 
-        binding.bottomMenu.menuSetting.setBackgroundResource(
-            R.drawable.bg_menu_selected
-        )
+        // Chuyển sang màn hình đổi mật khẩu
+        btnUpdatePassword.setOnClickListener {
 
-        binding.bottomMenu.menuSearch.setOnClickListener {
-            startActivity(Intent(this, SearchActivity::class.java))
-            finish()
-        }
-
-        binding.bottomMenu.menuSaved.setOnClickListener {
-            startActivity(Intent(this, SavedActivity::class.java))
-            finish()
-        }
-
-        binding.bottomMenu.menuHome.setOnClickListener {
-            startActivity(Intent(this, HomeActivity::class.java))
-            finish()
+            startActivity(
+                Intent(
+                    this,
+                    UpdatePasswordActivity::class.java
+                )
+            )
         }
 
         // Đăng xuất
-        binding.btnLogout.setOnClickListener {
+        btnLogout.setOnClickListener {
+
             FirebaseAuth.getInstance().signOut()
 
-            val intent = Intent(this, LoginActivity::class.java)
-            intent.flags =
-                Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(
+                Intent(
+                    this,
+                    LoginActivity::class.java
+                )
+            )
 
-            startActivity(intent)
             finish()
         }
     }
